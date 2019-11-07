c98b9e0dec9504d5413111a492742e0bc0b75f91
negris52 <negris@bk.ru>
2019-11-07 15:34:54 +0300

    some code-style edit

--- pages/admin_conf_page.rb
+++ pages/admin_conf_page.rb
@@ -6,5 +6,5 @@ class AdminConfPage
     @driver.open('http://admin:admin@192.168.3.63/alfresco/s/admin')
     @driver.click_text('ONLYOFFICE Configuration')
     @driver.find_id('onlyurl')
-    end
+  end
 end
